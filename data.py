154c7b2cd6bc23127cc317618f952fad78783a2b
Majda <mhamomi1@gmail.com>
2023-11-08 19:21:19 +0100

    modifdata

--- data.py
+++ data.py
@@ -1,63 +1,30 @@
-
-import openmeteo_requests
-import requests_cache
+import requests
 import pandas as pd
-from retry_requests import retry
 
-# Initialisation de la session de cache
-cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
+url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,wind_speed_10m_max"
 
-# Configuration des paramètres de retry
-retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+response = requests.get(url)
 
-# Initialisation du client Open-Meteo
-openmeteo = openmeteo_requests.Client(session=retry_session)
+if response.status_code == 200:
+    data = response.json()
 
-# Paramètres de l'API Open-Meteo
-url = "https://api.open-meteo.com/v1/forecast"
-params = {
-    "latitude": 52.52,
-    "longitude": 13.41,
-    "daily": ["temperature_2m_max", "temperature_2m_min", "precipitation_sum", "wind_speed_10m_max"]
-}
+    # Créez un DataFrame à partir des données
+    df = pd.DataFrame(data["daily"])
 
-# Appel de l'API Open-Meteo
-responses = openmeteo.weather_api(url, params=params)
+    # Définissez un dictionnaire pour mapper les anciens noms de colonnes aux nouveaux noms
+    column_mapping = {
+        "temperature_2m_max": "Temp Max (°C)",
+        "temperature_2m_min": "Temp Min (°C)",
+        "precipitation_sum": "Precipitation (mm)",
+        "wind_speed_10m_max": "Wind Speed Max (m/s)"
+    }
 
-# Vérification des réponses
-if not responses:
-    print("Aucune réponse de l'API Open-Meteo.")
-    exit()
+    # Renommez les colonnes en utilisant le dictionnaire de mappage
+    df = df.rename(columns=column_mapping)
 
-# Traitement de la première réponse (uniquement pour une seule localisation, ajoutez une boucle pour plusieurs localisations)
-response = responses[0]
+    # Affichez le DataFrame avec les nouveaux noms de colonnes
+    print(df)
+else:
+    print("Erreur lors de la requête. Statut de réponse :", response.status_code)
 
-# Affichage des informations de localisation
-print(f"Coordonnées {response.Latitude()}°E {response.Longitude()}°N")
-print(f"Altitude {response.Elevation()} m au-dessus du niveau de la mer")
-print(f"Fuseau horaire {response.Timezone()} ({response.TimezoneAbbreviation()})")
-print(f"Décalage par rapport à GMT+0 {response.UtcOffsetSeconds()} s")
 
-# Traitement des données quotidiennes
-daily = response.Daily()
-
-# Extraction des variables quotidiennes
-daily_data = {
-    "date": pd.date_range(
-        start=pd.to_datetime(daily.Time(), unit="s"),
-        end=pd.to_datetime(daily.TimeEnd(), unit="s"),
-        freq=pd.Timedelta(seconds=daily.Interval()),
-        closed="left"
-    ),
-    "temperature_max": daily.Variables(0).ValuesAsNumpy(),
-    "temperature_min": daily.Variables(1).ValuesAsNumpy(),
-    "precipitation": daily.Variables(2).ValuesAsNumpy(),
-    "wind_speed": daily.Variables(3).ValuesAsNumpy()
-}
-
-# Création d'un DataFrame pandas à partir des données quotidiennes
-daily_dataframe = pd.DataFrame(data=daily_data)
-daily_dataframe = round(daily_dataframe, 2)
-
-# Affichage du DataFrame
-print(daily_dataframe)
