4983b4baa383136fe08cb7904916e5d52b1933d3
Majda <mhamomi1@gmail.com>
2023-11-09 10:59:02 +0100

    commit3

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import quarto
 url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,wind_speed_10m_max"
 
 response = requests.get(url)
